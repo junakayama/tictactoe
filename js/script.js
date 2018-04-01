70dc4470908402a8aac930c445fffca4004a30be
Júlia Nakayama Silveira <junakayama97@gmail.com>
2018-04-01 16:45:17 -0300

    algoritmo minimax funcionando

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 var board = [0,1,2,3,4,5,6,7,8]; 
-var round = 0;
-var iter = 0;
 var computer = "x"
-var humanP = "o"
-var currentPlayer = humanP;
+var human = "o"
+var currentPlayer = human;
 
 
 const cells = document.querySelectorAll('.cell');
@@ -29,8 +27,7 @@ function turn(position,player){
     if(player==currentPlayer && board[position]==position){
         board[position] = player;
         cells[position].innerHTML = player
-        checkWinner(board, player);
-        round++;
+        if (checkWinner(board, player)) gameOver();
         nextPlayer();
     }
 }
@@ -44,94 +41,96 @@ function checkWinner(board,player){
         (board[2] == player && board[5] == player && board[8] == player) ||
         (board[0] == player && board[4] == player && board[8] == player) ||
         (board[2] == player && board[4] == player && board[6] == player)) {
-        gameOver();
+        return true;
     }
+    return false;
 }
 
 function nextPlayer(){
-    if(currentPlayer == humanP){
+    if(currentPlayer == human){
         console.log("computer turn");
         currentPlayer = computer;
     }else{
         console.log("human turn");
-        currentPlayer = humanP;
+        currentPlayer = human;
     }
 }
 
-function minmax(board,player){
-    iter++;
+function minimax(newBoard, player) {
+    var availableCells = emptyCells(newBoard);
 
-    var availableCells = emptyCells();
-
-    if(checkWinner(board,computer)){
-        return 1;   
-    }
-    if(checkWinner(board,humanP)){
-        return -1;
-    }
-    if(!checkWinner(board,computer)&&!checkWinner(board,humanP)){
-        return 0;
+    if (checkWinner(newBoard, human)) {
+        return {score:-1};
+    } else if (checkWinner(newBoard, computer)) {
+        return {score:1};
+    } else if (availableCells.length === 0) {
+        return {score: 0};
     }
 
-    moves = possibleMoves(board,availableCells,player);
+    var moves = possibleMoves(newBoard, availableCells, player);
 
+    var bestMove = getBestMove (moves, player);
+
+    return moves[bestMove];
 }
 
-function possibleMoves(newBoard,availableCells,player){
-    var moves = []
-    for (let index = 0; index < availableCells.length; index++){
-        var move = {
-            index: null,
-            score: null
-        };
+function possibleMoves(newBoard, availableCells, player) {
+    var moves = [];
+    for (var i = 0; i < availableCells.length; i++) {
+        var move = {}
+        move.index = newBoard[availableCells[i]];
+        newBoard[availableCells[i]] = player;
 
-        move.index = newBoard[availableCells[index]];
-        newBoard[availableCells[index]] = player;
-
-        if(player == computer){
-            var score = minmax(newBoard,humanP)
-            move.score = score;
-        }else{
-            var score = minmax(newBoard,computer)
-            move.score = score;
+        if (player == computer) {
+            var result = minimax(newBoard,human);
+            move.score = result.score;
+        } else {
+            var result = minimax(newBoard,computer);
+            move.score = result.score;
         }
 
-        newBoard[availableCells[index]] = move.index;
+        newBoard[availableCells[i]] = move.index;
         moves.push(move);
     }
+
     return moves;
 }
 
-function getBestMove(moves){
-    var bestMove;
-    searchBestMove(moves,computer);
-    searchBestMove(moves,humanP);
-}
 
-function searchBestMove(moves,player){
-    var bestScore = (player == computer) ? -10000 : 10000;
-    for (var i = 0; i < moves.length; i++) {
-        var condition = (player == computer) ? (moves[i].score > bestScore) : (moves[i].score < bestScore)
-        if (condition) {
-            bestScore = moves[i].score;
-            bestMove = i;
+function getBestMove(moves, player) {
+    var bestMove;
+    if (player == computer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
         }
     }
+
     return bestMove;
 }
 
 function emptyCells(){
-    return board.filter(cell => cell != humanP && cell != computer);
+    return board.filter(cell => cell != human && cell != computer);
 }
 
 function bestSpot() {
-    return emptyCells()[0];
+    return minimax(board, currentPlayer).index;
 }
 
 function reset(){
     board = [0,1,2,3,4,5,6,7,8]; 
-    round = 0;
-    currentPlayer = humanP;
+    currentPlayer = human;
 }
 
 function checkTie() {
@@ -153,5 +152,5 @@ function gameOver(tie) {
     for (var i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', turnClick, false);
     }
-    declareWinner(currentPlayer == humanP ? "You win!" : "You lose.")
+    declareWinner(currentPlayer == human ? "You win!" : "You lose.")
 }
--- script2.js
+++ script2.js
@@ -1,27 +1,178 @@
-var origBoard;
-const human = 'o';
-const computar = 'x';
-const winCombos = [
-	[0,1,2],
-	[3,4,5],
-	[0,3,6],
-	[1,4,7],
-	[2,5,8],
-	[0,4,8],
-	[6,4,2]
-];
+var board = [0,1,2,3,4,5,6,7,8]; 
+var round = 0;
+var iter = 0;
+var computer = "x"
+var humanP = "o"
+var currentPlayer = humanP;
+
+
 const cells = document.querySelectorAll('.cell');
+const replay = document.getElementById('replay');
 startGame();
 
-function startGame () {
-	origBoard = Array.from(Array(9).keys());
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].innerText = '';
-		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick, false);
-	};
+function startGame() {
+    reset();
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].addEventListener("click",turnClick,false);
+    }
+    replay.addEventListener("click",startGame,false);
 }
 
-function turnClick(square) {
-	console.log(square.target.id);
+function turnClick(cell) {
+    turn(cell.target.id,currentPlayer)
+    console.log(board)
+    if(!checkTie()) turn(bestSpot(), currentPlayer);
+}
+
+function turn(position,player){
+    if(player==currentPlayer && board[position]==position){
+        board[position] = player;
+        cells[position].innerHTML = player
+        if (checkWinner(board, player)) gameOver();
+        round++;
+        nextPlayer();
+    }
+}
+
+function checkWinner(board,player){
+    if ((board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)) {
+        return true;
+    }
+    return false;
+}
+
+function nextPlayer(){
+    if(currentPlayer == humanP){
+        console.log("computer turn");
+        currentPlayer = computer;
+    }else{
+        console.log("human turn");
+        currentPlayer = humanP;
+    }
+}
+
+// function minmax(board,player){
+//     iter++;
+
+//     var availableCells = emptyCells(board);
+
+//     if(checkWinner(board,computer)){
+//         return 1;   
+//     }
+//     if(checkWinner(board,humanP)){
+//         return -1;
+//     }
+//     if(!checkWinner(board,computer)&&!checkWinner(board,humanP)){
+//         return 0;
+//     }
+
+//     moves = possibleMoves(board,availableCells,player);
+
+// }
+
+function emptyCells(board){
+    return board.filter(cell => cell != humanP && cell != computer);
+}
+
+function bestSpot() {
+    return minimax(board, currentPlayer).index;
+}
+
+function minimax(newBoard, player) {
+	var availableCells = emptyCells(newBoard);
+
+	if (checkWinner(newBoard, humanP)) {
+		return {score:-1};
+	} else if (checkWinner(newBoard, computer)) {
+		return {score:1};
+	} else if (availableCells.length === 0) {
+		return {score: 0};
+	}
+
+	var moves = possibleMoves(newBoard, availableCells, player);
+
+	var bestMove = getBestMove (moves, player);
+
+	return moves[bestMove];
+}
+
+function getBestMove(moves, player) {
+	var bestMove;
+	if (player == computer) {
+		var bestScore = -10000;
+		for (var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for (var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return bestMove;
+}
+
+function possibleMoves(newBoard, availableCells, player) {
+	var moves = [];
+	for (var i = 0; i < availableCells.length; i++) {
+		var move = {}
+		move.index = newBoard[availableCells[i]];
+		newBoard[availableCells[i]] = player;
+
+		if (player == computer) {
+			var result = minimax(newBoard,humanP);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard,computer);
+			move.score = result.score;
+		}
+
+		newBoard[availableCells[i]] = move.index;
+		moves.push(move);
+	}
+
+	return moves;
+}
+
+function reset(){
+    board = [0,1,2,3,4,5,6,7,8]; 
+    round = 0;
+    currentPlayer = humanP;
+}
+
+function checkTie() {
+    if (emptyCells(board).length == 0) {
+        for (var i = 0; i < cells.length; i++) {
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie game!")
+        return true;
+    } 
+    return false;
+}
+
+function declareWinner(who) {
+    console.log(who);
+}
+
+function gameOver(tie) {
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner(currentPlayer == humanP ? "You win!" : "You lose.")
 }
